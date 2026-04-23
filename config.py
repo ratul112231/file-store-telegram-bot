09e53473aa496a15fd88b42a50ac27c01638cad3
ratul112231 <hosainratul25@gmail.com>
2026-04-23 23:33:15 +0600

    Update Telegram API credentials and channel IDs

--- config.py
+++ config.py
@@ -2,23 +2,23 @@
 
 # --- REQUIRED: Fill these in ---
 # Telegram API Credentials (from my.telegram.org)
-API_ID = 8328942211 # Replace with your API ID
-API_HASH = "YOUR_API_HASH"
-BOT_TOKEN = "YOUR_BOT_API_TOKEN"
+API_ID = 39396720 # Replace with your API ID
+API_HASH = "945f0314b982ab0847fd009e5e447b64"
+BOT_TOKEN = "8222385318:AAH6AK3nSOX2CPxLNAr9CQtqhJZfM-8Jhro"
 
 # Your private channel ID where files are stored
-DB_CHANNEL = -88994477333  # Replace with your channel ID
+DB_CHANNEL = -1003656239689  # Replace with your channel ID
 
 # Your user ID and any other admin user IDs
-ADMINS = [123456789, 987654321]  # Replace with actual Telegram user IDs
+ADMINS = [6992010963, 7831735222]  # Replace with actual Telegram user IDs
 
 # Your API token from ShrinkEarn.com
 SHORTENER_API = "YOUR_SHRINKEARN_API_TOKEN"
 
 # --- DEFAULT SETTINGS (Can be changed from the admin panel) ---
-DEFAULT_UPI_HANDLE = "your-upi-id@okhdfcbank"
-DEFAULT_QR_CODE_URL = "https://i.ibb.co/g9fXy3x/image.png"
-DEFAULT_PAYMENT_LINK = "https://upi.pe/your-link" # Example Link
+DEFAULT_bkash_HANDLE = "your-bkash-id@RatulHosain"
+DEFAULT_QR_CODE_URL = "https://qr.bka.sh/281014021VIVv0szr0EB6CCB5C"
+DEFAULT_PAYMENT_LINK = "https://qr.bka.sh/281014021VIVv0szr0EB6CCB5C" # Example Link
 DEFAULT_AUTO_DELETE_MINUTES = 10 # Default time in minutes for files to be deleted after sending
 
 # Your username for users to send payment screenshots to
